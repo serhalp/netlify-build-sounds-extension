fcf1108a7a9bdec0766d6eaad36b5fa6343e2b55
Philippe Serhal <philippe.serhal@netlify.com>
2025-03-13 14:42:13 -0400

    wip: try remove build event handler

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 // Documentation: https://sdk.netlify.com/docs
 import { NetlifyExtension } from "@netlify/sdk";
 
-
 const extension = new NetlifyExtension();
 
-extension.addBuildEventHandler("onPreBuild", () => {
-  // If the build event handler is not enabled, return early
-  if (!process.env["NETLIFY_BUILD_SOUNDS_EXTENSION_ENABLED"]) {
-    return;
-  }
-  console.log("Hello there.");
-});
-  
+// extension.addBuildEventHandler("onPreBuild", () => {
+//   // If the build event handler is not enabled, return early
+//   if (!process.env["NETLIFY_BUILD_SOUNDS_EXTENSION_ENABLED"]) {
+//     return true;
+//   }
+//   console.log("Hello there.");
+// });
+//
 export { extension };
-
